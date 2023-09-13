f8ad2d3ae6e64d9c0d10d31a35d16517581ff329
Gaurav <grv33431@gmail.com>
2023-09-13 08:08:47 +0530

    fav done 👍

--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -3,51 +3,56 @@ import useAppContext from "../../useAppContext";
 import { getUserFavorites } from "../../databse";
 import { getMovieById } from "../../api";
 import MovieCard from "../components/MovieCard";
+import favimg from "../../assets/nofav.png";
 const Favorites = () => {
-    const [favorites, setFavorites] = useState([]);
-    const [movies, setMovies] = useState([]);
-    const { user } = useAppContext();
-    useEffect(() => {
-        try {
-            (async function () {
-                if (favorites.length > 0) {
-                    let moviesArray = [];
-                    for (const favorite of favorites) {
-                        console.log("look here", favorite);
-                        let { data, error } = await getMovieById(favorite);
-                        if (!error) {
-                            moviesArray.push(data);
-                        }
-                    }
-                    setMovies(moviesArray);
-                }
-            })();
-        } catch (error) {
-            console.log(error);
+  const [favorites, setFavorites] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const { user } = useAppContext();
+  useEffect(() => {
+    try {
+      (async function () {
+        if (favorites.length > 0) {
+          let moviesArray = [];
+          for (const favorite of favorites) {
+            console.log("look here", favorite);
+            let { data, error } = await getMovieById(favorite);
+            if (!error) {
+              moviesArray.push(data);
+            }
+          }
+          setMovies(moviesArray);
         }
-    }, [favorites]);
-    useEffect(() => {
-        try {
-            (async function () {
-                if (user) {
-                    let result = await getUserFavorites(user);
-                    if (result) {
-                        setFavorites(result);
-                    }
-                }
-            })();
-        } catch (error) {
-            console.log(error);
+      })();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [favorites]);
+  useEffect(() => {
+    try {
+      (async function () {
+        if (user) {
+          let result = await getUserFavorites(user);
+          if (result) {
+            setFavorites(result);
+          }
         }
-    }, []);
+      })();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
-    return (
-        <div className="favorites-page">
-            {movies.map((movie) => {
-                return <MovieCard key={movie.id} movie={movie} />;
-            })}
-        </div>
-    );
+  return (
+    <div className="favorites-page">
+      {movies.length > 0 ? (
+        movies.map((movie) => {
+          return <MovieCard key={movie.id} movie={movie} />;
+        })
+      ) : (
+        <img src={favimg} className="nofavimg" />
+      )}
+    </div>
+  );
 };
 
 export default Favorites;
